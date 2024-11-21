0acc15bd349ef685c3c577bd0e2350883e993958
backfireIBGM <thebeast4028@gmail.com>
2024-11-21 10:27:21 -0800

    split processFeed up so it now calls 4 sub-funtions processSearchFeed processLeftFeed processMiddleFeed processRightFeed

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ async function fetchAllRSSFeeds(proxyUrl, rssUrls) {
     try {
         // Create an array of promises for all RSS feeds
         const feedPromises = rssUrls.map(url => fetchRSSWithProxy(proxyUrl, url));
-        console.log(feedPromises);
-        
+       // console.log(feedPromises);
+
         const results = await Promise.all(feedPromises); // Wait for all promises to resolve
 
         // Process each feed
@@ -33,7 +33,7 @@ async function fetchAllRSSFeeds(proxyUrl, rssUrls) {
             const feedItems = extractFeedItems(xmlDoc);
             allFeedItems.push(...feedItems);  // Push the current feed items
         }
-        
+
         return allFeedItems;
     } catch (error) {
         console.error('Error fetching RSS feeds:', error);
@@ -54,25 +54,103 @@ function extractFeedItems(xmlDoc) {
     for (let i = 0; i < entries.length; i++) {
         const entry = entries[i];
         feedItems.push({
+            channelId: entry.querySelector('channelId')?.textContent,
             id: entry.querySelector('videoId')?.textContent,
             title: entry.querySelector('title')?.textContent,
             link: entry.querySelector('link')?.getAttribute('href'),
             published: entry.querySelector('published')?.textContent,
             thumbnail: entry.querySelector('thumbnail')?.getAttribute('url'),
             thumbnailWidth: entry.querySelector('thumbnail')?.getAttribute('width'),
-            thumbnailHeight: entry.querySelector('thumbnail')?.getAttribute('height')
+            thumbnailHeight: entry.querySelector('thumbnail')?.getAttribute('height'),
+            description: entry.querySelector('media\\:descriotion, description')?.textContent
         });
     }
 
-    console.log("feedItems");
+    // console.log("feedItems");
     return feedItems;
 }
 
-function processFeed(feedItems) {
-    // Get the feed container
-    const feedContainer = document.getElementById('feed');
+document.getElementById('searchBox').addEventListener('keyup', function () {
+    processFeed(allFeedItems, this.value.toLowerCase());
+});
+
+// let allFeedItems = [];
+
+function processFeed(feedItems, isSearch) {
+    if (isSearch) {
+        processSearchFeed(feedItems, isSearch)
+    }
+    else {
+        processLeftFeed(feedItems)
+        processMiddleFeed(feedItems)
+        processRightFeed(feedItems)
+    }
+
+
+
+
+
+
+
+    // const SearchBox = document.createElement('input');
+    // SearchBox.type = 'SearchBox';
+    // SearchBox.id = 'SearchBox-${uten.id}';
+
     
+
+
+    // console.log(feedItems);
+
+
+    // if (isSearch) {
+
+    // }
+    // else {
+
+    // }
+}
+
+function processSearchFeed(feedItems, isSearch) {
+    const searchFeed = document.getElementById('searchFeed');
+    searchFeed.innerHTML = '';
+
+    let searchResults = []
+
+    for (let key in feedItems) {
+        if (feedItems[key].title.toLowerCase().includes(isSearch) || feedItems[key].description.toLowerCase().includes(isSearch)) {
+            searchResults.push(feedItems[key]);
+        }
+
+       // console.log(isSearch);
+    }
+
     // Format date function
+    const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+}
+}
+
+
+
+function processLeftFeed(feedItems) {
+    const leftFeedContainer = document.getElementById('leftFeed');
+    leftFeedContainer.innerHTML = '';
+
+    let leftResults = []
+
+    for (channelKey in feedItems) {
+        if (feedItems[channelKey].channelId.includes("UCSUu1lih2RifWkKtDOJdsBA")) {
+            leftResults.push(feedItems[channelKey]);
+        }
+    }
+
     const formatDate = (dateString) => {
         const date = new Date(dateString);
         return date.toLocaleDateString('en-US', {
@@ -82,10 +160,10 @@ function processFeed(feedItems) {
             hour: '2-digit',
             minute: '2-digit'
         });
-    };
+    }
 
-    // Process each feed item
-    feedItems.forEach(item => {
+
+    leftResults.forEach(item => {
         // Create card element
         const card = document.createElement('div');
         card.className = 'card';
@@ -120,16 +198,145 @@ function processFeed(feedItems) {
         card.appendChild(thumbnailLink);
         card.appendChild(title);
         card.appendChild(published);
-        feedContainer.appendChild(card);
+        leftFeedContainer.appendChild(card);
     });
 }
+
+function processMiddleFeed(feedItems) {
+    const miiddFeedContainer = document.getElementById('middleFeed');
+    miiddFeedContainer.innerHTML = '';
+
+    let middleResults = []
+
+    for (channelKey in feedItems) {
+        if (feedItems[channelKey].channelId.includes("UC6uKrU_WqJ1R2HMTY3LIx5Q")) {
+            middleResults.push(feedItems[channelKey]);
+        }
+    }
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+    }
+
+    middleResults.forEach(item => {
+        // Create card element
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        // Create title with link
+        const title = document.createElement('h2');
+        title.className = 'title';
+
+        const link = document.createElement('a');
+        link.href = item.link;
+        link.textContent = item.title;
+        link.onmouseover = () => link.style.textDecoration = 'underline';
+        link.onmouseout = () => link.style.textDecoration = 'none';
+        link.target = "_blank";
+
+        const published = document.createElement('span'); // Create published date span
+        published.textContent = formatDate(item.published);
+        published.className = 'date';
+
+        const thumbnail = document.createElement('img');
+        thumbnail.src = item.thumbnail;
+        thumbnail.alt = item.title;
+
+        const thumbnailLink = document.createElement('a');
+        thumbnailLink.href = link.href;
+        thumbnailLink.target = "_blank";
+        thumbnailLink.appendChild(thumbnail);
+
+
+        // Assemble the card
+        title.appendChild(link);
+        card.appendChild(thumbnailLink);
+        card.appendChild(title);
+        card.appendChild(published);
+        miiddFeedContainer.appendChild(card);
+    });
+}
+
+function processRightFeed(feedItems) {
+    const rightFeedContainer = document.getElementById('rightFeed');
+    rightFeedContainer.innerHTML = '';
+
+    let rightResults = []
+
+    for (channelKey in feedItems) {
+        if (feedItems[channelKey].channelId.includes("UCy6Q9UCG7Wa-N7nht2BFrHA")) {
+            rightResults.push(feedItems[channelKey]);
+        }
+    }
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        return date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+    }
+
+    rightResults.forEach(item => {
+        // Create card element
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        // Create title with link
+        const title = document.createElement('h2');
+        title.className = 'title';
+
+        const link = document.createElement('a');
+        link.href = item.link;
+        link.textContent = item.title;
+        link.onmouseover = () => link.style.textDecoration = 'underline';
+        link.onmouseout = () => link.style.textDecoration = 'none';
+        link.target = "_blank";
+
+        const published = document.createElement('span'); // Create published date span
+        published.textContent = formatDate(item.published);
+        published.className = 'date';
+
+        const thumbnail = document.createElement('img');
+        thumbnail.src = item.thumbnail;
+        thumbnail.alt = item.title;
+
+        const thumbnailLink = document.createElement('a');
+        thumbnailLink.href = link.href;
+        thumbnailLink.target = "_blank";
+        thumbnailLink.appendChild(thumbnail);
+
+
+        // Assemble the card
+        title.appendChild(link);
+        card.appendChild(thumbnailLink);
+        card.appendChild(title);
+        card.appendChild(published);
+        rightFeedContainer.appendChild(card);
+    });
+}
+
+document.getElementById('searchBox').addEventListener('change', function () {
+    processFeed(allFeedItems, this.checked);
+});
+
 
 const proxyUrl = 'https://corsproxy.io/';
 
 fetchAllRSSFeeds(proxyUrl, rssUrl)
     .then(feedItems => {
-        processFeed(feedItems);
-        console.log(feedItems);
+        allFeedItems = feedItems;
+        processFeed(feedItems, document.getElementById('searchBox').checked);
     })
     .catch(error => {
         console.error('Error:', error);
