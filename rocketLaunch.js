a6b26c21885e9c7c911bc15c0fe4d75cfe165fc8
backfireIBGM <thebeast4028@gmail.com>
2024-12-03 11:08:53 -0800

    dev next lauch count down

--- rocketLaunch.js
+++ rocketLaunch.js
@@ -17,6 +17,7 @@ fetchLaunches(proxyUrl, jsonRocketLaunches)
             // console.log(data);
     
             processLaunches(data);
+            setDateToLaunch(data);
         } catch (error) {
             console.error('Error fetching launches:', error);
         }
@@ -30,7 +31,7 @@ fetchLaunches(proxyUrl, jsonRocketLaunches)
         launchesFeed.innerHTML = '';
         const textbox = document.createElement('textarea');
         textbox.style.width = '100%';
-        textbox.style.height = '200px';
+        // textbox.style.height = '200px';
         textbox.style.backgroundColor = '#111';
         textbox.style.color = 'white';
         textbox.style.padding = '10px';
@@ -49,3 +50,44 @@ fetchLaunches(proxyUrl, jsonRocketLaunches)
         textbox.value = launchText;
         launchesFeed.appendChild(textbox);
     }
+    
+    let targetDate;
+    
+    function setDateToLaunch(data) {
+        const date = new Date(); // New Date object
+        date.setDate(date.getDate() + 30) // Current day plus 30 days
+        targetDate = date;
+        
+
+        targetDate = new Date (data.result[0].t0);
+        console.log(targetDate, data);
+        // targetDate = date;
+
+        // const d = new Date("October 13, 2014 11:13:00");
+    }
+
+// convert target date to a date object
+
+
+
+        // set up variable to manage date rules
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+
+// set up the loop to update the countdown once a second
+let x = setInterval(function() {
+
+  // get the current time
+  let now = new Date().getTime();
+  
+  // get the time remaining
+  let distance = targetDate - now;
+
+  // update the <span> tags with the countdown values
+  document.querySelector('#days').innerHTML = Math.floor(distance / (day))+" days, ";
+  document.querySelector('#hours').innerHTML = Math.floor((distance % (day)) / (hour))+" hours, ";
+  document.querySelector('#minutes').innerHTML = Math.floor((distance % (hour)) / (minute))+" minutes, ";
+  document.querySelector('#seconds').innerHTML = Math.floor((distance % (minute)) / second)+" seconds ";
+}, second)
